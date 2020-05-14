12bc59c1bc381c11af653c85317ee5daaa2a0b24
Ankit Kumar Singh <ankit1.singh@igglobal.com>
2020-05-14 16:41:51 +0530

    Role creation Commit

---
+++ WebApps/Controllers/AdministratorController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApps.ViewModels;
+
+namespace WebApps.Controllers
+{
+    [Authorize]
+    public class AdministratorController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministratorController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid) 
+            {
+                IdentityRole identityRole = new IdentityRole() 
+                {
+                      Name = model.RoleName,
+                };
+                IdentityResult identityResult = await roleManager.CreateAsync(identityRole);
+                
+                if(identityResult.Succeeded) 
+                {
+                    return RedirectToAction("index", "home");
+                }
+                foreach (IdentityError item in identityResult.Errors)
+                {
+                    ModelState.AddModelError("",  item.Description);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles() 
+        {
+            string _roleList = "No Roles Available";
+            var role = roleManager.Roles;
+            List<string> dataList = new List<string>();
+            if (role.Count() != 0)
+            {
+                _roleList = String.Empty;
+               // _roleList = role.Select(x => x.Name).ToList().ToString();
+                foreach (var item in role)
+                {
+                    _roleList = _roleList + " " + item.Name;
+                }
+            }
+            return Content(_roleList);
+        }
+    }
+}
--- WebApps/wwwroot/js/site.js
+++ WebApps/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+
+    debugger;
+    $('#UseShipAddr').click(function () {
+        debugger;
+        $.ajax({
+            url: "https://localhost:44371/administrator/ListRoles",
+            type: "GET",
+            cache: false,
+            async: true,
+            success: function (data) {
+                var splitRoles = data.split(" ");
+                alert(splitRoles)
+                var len = data.split(",").filter((i) => i.length).length;
+                debugger;
+            },
+            failure: function (data) {
+                alert(data.responseText);
+            }, //End of AJAX failure function   myrole card-title mt-3
+            error: function (data) {
+                alert(data.responseText);
+            } //End of AJAX error function  
+        });
+    });
+});
+
+
+
